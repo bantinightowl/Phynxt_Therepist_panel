996da3b0c06b15146c0eb45f5d86da7c0e218522
bantinightowl <bantikumar@digitalnightowl.in>
2025-09-18 10:49:10 +0530

    commit change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
-// tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // or 'class' (remove "false")
+  theme: { extend: {} },
   plugins: [],
 }
